92c04c2513fe562bddb6926de3d2e766309db15d
Anastazja <nastazjaw98@gmail.com>
2021-08-31 11:51:56 +0200

    add style and todo divs

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('done');
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Functions
+const addTodo = (event) => {
+  event.preventDefault();
+  // todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create li
+  const newTodo = document.createElement("li");
+  newTodo.innerHTML = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //check mark button
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML ='<i class="fas fa-check"></li';
+  completeButton.classList.add('complete-btn');
+  todoDiv.appendChild(completeButton);
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML ='<i class="fas fa-trash"></li';
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv);
+};
+
+
+//Events
+todoButton.addEventListener("click", addTodo);
+
+
